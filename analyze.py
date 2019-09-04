b96ebb2ece10b755f1cdfc6f5916a2df83e59c61
charles <charles@Charless-MacBook.local>
2019-09-03 22:24:33 -0500

    installed Plotly dash

--- analyze.py
+++ analyze.py
@@ -1,10 +1,13 @@
 import numpy as np
 import pandas as pd
 import scipy as sp
+import scipy.integrate as spi
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 
 accelerometer_df = pd.read_csv("data/Driving - Recording 1.csv")
-print(accelerometer_df.head(5))
-print(accelerometer_df.timestamp)
-print(accelerometer_df.AccY)
-vel = np.trapz(accelerometer_df.AccY, accelerometer_df.timestamp)
+
+vel = spi.cumtrapz(accelerometer_df.AccY, x=accelerometer_df.timestamp)
 print(vel)
+
